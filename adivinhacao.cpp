2cc061caa2042db1b0c5e217bf48b7a97bf38147
Anna Moraco <annamoraco@gmail.com>
2021-04-23 18:14:44 -0300

    Adicionando níveis de dificuldade

--- adivinhacao.cpp
+++ adivinhacao.cpp
@@ -6,7 +6,6 @@ int main(){
     int chute;
     const int NUMEROSECRETO = 42;
     bool acertou = false;
-    int tentativas = 0;
 
     double pontos = 1000.0;
 
@@ -14,14 +13,24 @@ int main(){
     cout << "** Bem vindo ao jogo da adivinhacao! **" << endl;
     cout << "***************************************" << endl << endl; 
 
-    while(!acertou){
+    cout << "Escolha o nivel de dificuldade: " << endl ;
+    cout << "Facil (F) Medio (M) Dificil (D)" << endl << endl;
+    
+    char dificuldade;
+    cin >> dificuldade;
 
-        tentativas++;
+    int numerotentativas;
+
+    if (dificuldade == 'F') numerotentativas = 30;
+    else if (dificuldade == 'M' ) numerotentativas = 20;
+    else numerotentativas = 10;
+
+    for (int tentativas = 1; tentativas <= numerotentativas; tentativas++){
 
         bool chutevalido = false;
 
         while(!chutevalido){
-            cout << "Tentativa " << tentativas << endl << endl;
+            cout << "Tentativa " << tentativas << " de " << numerotentativas << endl << endl;
 
             cout << "Digite um numero positivo: " ;
             cin  >> chute ;
@@ -42,7 +51,12 @@ int main(){
     }
     cout << "FIM DE JOGO" << endl << endl;
     
-    cout.precision(1);
-    cout << fixed;
-    cout << "Pontuacao obtida: " << pontos << endl << endl;
+    if (acertou){
+        cout.precision(1);
+        cout << fixed;
+        cout << "Parabens, voce ganhou!" << endl ;
+        cout << "Pontuacao obtida: " << pontos << endl << endl;
+    } else {
+        cout << "Voce perdeu!" << endl;
+    }
 }
