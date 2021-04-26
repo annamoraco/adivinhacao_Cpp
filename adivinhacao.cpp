b35f5a46950b7c7adc51b10dac8e71336108a59f
Anna Moraco <annamoraco@gmail.com>
2021-04-26 14:26:47 -0300

    Quebrando a main em várias funções

--- adivinhacao.cpp
+++ adivinhacao.cpp
@@ -3,19 +3,13 @@
 #include <ctime>
 using namespace std;
 
-int main(){
-
-    int chute;
-    srand(time(NULL));
-    const int NUMEROSECRETO = rand()%100;
-    bool acertou = false;
-
-    double pontos = 1000.0;
-
+void cabecalho(){
     cout << endl << "***************************************" << endl;
     cout << "** Bem vindo ao jogo da adivinhacao! **" << endl;
     cout << "***************************************" << endl << endl; 
+}
 
+int seleciona_dificuldade(){
     cout << "Escolha o nivel de dificuldade: " << endl ;
     cout << "Facil (F) Medio (M) Dificil (D)" << endl << endl;
     
@@ -27,39 +21,69 @@ int main(){
     if (dificuldade == 'F') numerotentativas = 30;
     else if (dificuldade == 'M' ) numerotentativas = 20;
     else numerotentativas = 10;
+    return numerotentativas;
+}
 
-    for (int tentativas = 1; tentativas <= numerotentativas; tentativas++){
+int recebe_validachute(int tentativas, int numerotentativas){
+    bool chutevalido = false;
+    int chute;
+    while(!chutevalido){        
+        cout << "Tentativa " << tentativas << " de " << numerotentativas << endl << endl;
 
-        bool chutevalido = false;
+        cout << "Digite um numero positivo: " ;
+        cin  >> chute ;
+        if (chute > 0 ) chutevalido = true;
+    };
+    return chute;
+}
 
-        while(!chutevalido){
-            cout << "Tentativa " << tentativas << " de " << numerotentativas << endl << endl;
-
-            cout << "Digite um numero positivo: " ;
-            cin  >> chute ;
-            if (chute > 0 ) chutevalido = true;
-        };
-
-        acertou = chute == NUMEROSECRETO;
+bool verifica_chute(int chute, int NUMEROSECRETO, int* tentativas, int numerotentativas){
+        bool acertou = chute == NUMEROSECRETO;
         bool maior = chute > NUMEROSECRETO;
 
         if (acertou){
             cout << "Parabens! Voce acertou o numero secreto!" << endl << endl;
-            break;
+            *tentativas = numerotentativas;
+            return true;
         } else if (maior){
             cout << "O numero secreto eh menor." << endl << endl;
         } else {
             cout << "O numero secreto eh maior." << endl << endl;
         }
+        return false;
+}
+
+void ganhou(double pontos){
+        cout.precision(1);
+        cout << fixed;
+        cout << "Parabens, voce ganhou!" << endl ;
+        cout << "Pontuacao obtida: " << pontos << endl << endl;
+}
+
+int main(){
+
+    int chute;
+    srand(time(NULL));
+    const int NUMEROSECRETO = rand()%100;
+    bool acertou = false;
+    double pontos = 1000.0;
+
+    cabecalho();
+
+    int numerotentativas = seleciona_dificuldade();
+
+    for (int tentativas = 1; tentativas <= numerotentativas; tentativas++){
+
+        int chute = recebe_validachute(tentativas, numerotentativas);
+
+        acertou = verifica_chute(chute, NUMEROSECRETO, &tentativas, numerotentativas);
+
         pontos = pontos - abs(NUMEROSECRETO-chute)/2.0;
     }
     cout << "FIM DE JOGO" << endl << endl;
     
     if (acertou){
-        cout.precision(1);
-        cout << fixed;
-        cout << "Parabens, voce ganhou!" << endl ;
-        cout << "Pontuacao obtida: " << pontos << endl << endl;
+        ganhou(pontos);
     } else {
         cout << "Voce perdeu. Tente novamente." << endl;
     }
